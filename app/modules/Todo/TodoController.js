2e7bef9661aaf36b1d5d1f76b8d11b4c9af3c68e
nbappi <bappi.cse562@gmail.com>
2016-09-18 17:25:48 +0600

    Rearrange add edit position

--- app/modules/Todo/TodoController.js
+++ app/modules/Todo/TodoController.js
@@ -3,25 +3,40 @@
 todoApp.controller('TodoController', ['$scope', '$http','todoFactory',
     function($scope, $http, todoFactory)
     {
+        $scope.details = false;
+        $scope.addArea = false;
         $scope.todos = todoFactory.get();
 
-        $scope.addItem = function(todo)
-        {
-            todoFactory.add(todo);
+        $scope.newForm = function(){
+           $scope.addArea = true;
+           $scope.details = false;
+           $scope.save = true;
+           $scope.edit = false;
 
-            $scope.todo = {
+           $scope.todo = {
                 country : '',
                 code : '',
                 city : ''
             };
         };
 
-        $scope.editItem = function(todoId){
-            $scope.todo = todoFactory.detail(todoId);
+        $scope.addItem = function(todo)
+        {
+            todoFactory.add(todo);
+            $scope.todo = {
+                country : '',
+                code : '',
+                city : ''
+            };
+            $scope.save = true;
         };
 
         $scope.editItem = function(todoId){
             $scope.todo = todoFactory.detail(todoId);
+            $scope.save = false;
+            $scope.edit = true;
+            $scope.addArea = true;
+            $scope.details = false;
         };
 
         $scope.updateItem = function( object, todoId){
@@ -30,10 +45,14 @@ todoApp.controller('TodoController', ['$scope', '$http','todoFactory',
 
         $scope.detailItem = function(todoId){
             $scope.todoDetail = todoFactory.detail(todoId);
+            $scope.details = true;
+            $scope.addArea = false;
         };
 
         $scope.deleteItem = function(data){
-            todoFactory.delete(data);
+           $scope.details = false;
+           $scope.addArea = false;
+           todoFactory.delete(data);
         };
     }
 ]);
