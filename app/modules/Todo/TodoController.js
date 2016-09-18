09a24364780d365034da5a4cf804ca569afa9b39
nbappi <bappi.cse562@gmail.com>
2016-09-18 16:46:51 +0600

    Show todo details

--- app/modules/Todo/TodoController.js
+++ app/modules/Todo/TodoController.js
@@ -20,9 +20,16 @@ todoApp.controller('TodoController', ['$scope', '$http','todoFactory',
             $scope.todo = todoFactory.detail(todoId);
         };
 
+        $scope.editItem = function(todoId){
+            $scope.todo = todoFactory.detail(todoId);
+        };
+
         $scope.updateItem = function( object, todoId){
             todoFactory.update( object, todoId);
-            $scope.title = ''; $scope.description = '';  $scope.save = true; $scope.edit = false;
+        };
+
+        $scope.detailItem = function(todoId){
+            $scope.todoDetail = todoFactory.detail(todoId);
         };
 
         $scope.deleteItem = function(data){
